a0efa8902b150c801bc7276e118ef5ecd4600cd4
Matthew Sanchez <MSanchez@tql.com>
2021-05-28 13:22:55 -0400

    Updated

--- SalesLibrary/CustomerController.cs
+++ SalesLibrary/CustomerController.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+
 using System.Text;
+using Microsoft.Data.SqlClient;
 using SalesLibrary;
 
 namespace SalesDbProject
@@ -11,16 +12,112 @@ namespace SalesDbProject
 
         private static Connection connection { get; set; }
 
-
-
-
-
-        public bool Delete(Customer customer)
+        public CustomerController(Connection connection)
         {
-            var sql = $"Delete from Customers " +
-             "Where Id = @Id ";
+            CustomerController.connection = connection;
+        }
+
+
+
+        public bool Create(Customer customer)
+        {
+            var sql = $"INSERT into Customers " +
+                " (Name, City, State, Sales, Active) " +
+                " VALUES " + $" (@name, @city, @state, @sales, " +
+                $" @active); ";
             var sqlcmd = new SqlCommand(sql, connection.SqlConn);
-            sqlcmd.Parameters.AddWithValue("@id", request.Id);
+            sqlcmd.Parameters.AddWithValue("@name", customer.Name);
+            sqlcmd.Parameters.AddWithValue("@city", customer.City);
+            sqlcmd.Parameters.AddWithValue("@state", customer.State);
+            sqlcmd.Parameters.AddWithValue("@sales", customer.Sales);
+            sqlcmd.Parameters.AddWithValue("@active", customer.Active);
+            var rowsAffected = sqlcmd.ExecuteNonQuery();
+
+            return (rowsAffected == 1);
+
+        }
+
+
+
+        public Customer GetByPk(int id)
+        {
+
+            var sql = $"SELECT * From Customers where id = {id}";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+            var sqldatareader = sqlcmd.ExecuteReader();
+            if (!sqldatareader.HasRows)
+            {
+                return null;
+            }
+            sqldatareader.Read();
+            var user = new Customer()
+            {
+                Id = Convert.ToInt32(sqldatareader["Id"]),
+                Name = Convert.ToString(sqldatareader["Mane"]),
+                City = Convert.ToString(sqldatareader["City"]),
+                State = Convert.ToString(sqldatareader["State"]),
+                Sales = Convert.ToDecimal(sqldatareader["Sales"]),
+                Active = Convert.ToBoolean(sqldatareader["Active"]),
+          
+            };
+            sqldatareader.Close();
+            return user;
+
+        }
+
+
+
+        public List<Customer> GetAllCustomers()
+        {
+
+            var sql = "SELECT * from Customers;";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+
+            var sqldatareader = sqlcmd.ExecuteReader();
+            var customer = new List<Customer>();
+            while (sqldatareader.Read())
+            {
+                var id = Convert.ToInt32(sqldatareader["Id"]);
+                var name = Convert.ToString(sqldatareader["Name"]);
+                var city = Convert.ToString(sqldatareader["City"]);
+                var state = Convert.ToString(sqldatareader["State"]);
+                var sales = Convert.ToDecimal(sqldatareader["Sales"]);
+                var active = Convert.ToBoolean(sqldatareader["Name"]);
+                var Customer = new Customer()
+                {
+                    Id = id,
+                    Name = name,
+                    City = city,
+                    State = state,
+                    Sales = sales,
+                    Active = active
+                };
+                customer.Add(Customer);
+
+            }
+            sqldatareader.Close();
+            return customer;
+
+        }
+
+
+
+
+        public bool Change(Customer customer)
+        {
+            var sql = $"Update Customers Set" +
+                " Name = @name, " +
+                "City = @city, " +
+                "State = @state, " +
+                "Sales = @sales, " +
+                "Active = @active, " +
+                "Where Id = @id;";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+            sqlcmd.Parameters.AddWithValue("@name", customer.Name);
+            sqlcmd.Parameters.AddWithValue("@city", customer.City);
+            sqlcmd.Parameters.AddWithValue("@state", customer.State);
+            sqlcmd.Parameters.AddWithValue("@sales", customer.Sales);
+            sqlcmd.Parameters.AddWithValue("@active", customer.Active);
             var rowsAffected = sqlcmd.ExecuteNonQuery();
             return (rowsAffected == 1);
 
@@ -29,27 +126,16 @@ namespace SalesDbProject
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+        public bool Delete(Customer customer)
+        {
+            var sql = $"Delete from Customers" +
+                "Where Id = @id";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+            sqlcmd.Parameters.AddWithValue("@id", customer.Id);
+            var rowsAffected = sqlcmd.ExecuteNonQuery();
+            return (rowsAffected == 1);
+ 
+        }
 
 
 
--- SalesLibrary/OrderLineController.cs
+++ SalesLibrary/OrderLineController.cs
@@ -1,15 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Microsoft.Data.SqlClient;
+using SalesDbProject;
 
 namespace SalesLibrary
 {
-   public class OrderLineController
+    public class OrderLineController
     {
 
+        private static Connection connection { get; set; }
+
+
+        public OrderLineController(Connection connection)
+        {
+            OrderLineController.connection = connection;
+        }
 
 
 
+        public bool Create(Orderline orderline)
+        {
+            var sql = $"INSERT into Orderlines " +
+                " (OrdersId, Product, Description, Quantity, Price) " +
+                " VALUES " + $" (@orderid, @product, @description, @quantity, " +
+                $" @price); ";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+            sqlcmd.Parameters.AddWithValue("@ordersid", orderline.OrdersId);
+            sqlcmd.Parameters.AddWithValue("@product", orderline.Product);
+            sqlcmd.Parameters.AddWithValue("@description", orderline.Description);
+            sqlcmd.Parameters.AddWithValue("@quantity", orderline.Quantity);
+            sqlcmd.Parameters.AddWithValue("@price", orderline.Price);
+            var rowsAffected = sqlcmd.ExecuteNonQuery();
+
+            return (rowsAffected == 1);
+
+        }
+
+
+
+        public Orderline GetByPk(int id)
+        {
+
+            var sql = $"SELECT * From OrderLines where id = {id}";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+            var sqldatareader = sqlcmd.ExecuteReader();
+            if (!sqldatareader.HasRows)
+            {
+                return null;
+            }
+            sqldatareader.Read();
+            var user = new Orderline()
+            {
+                Id = Convert.ToInt32(sqldatareader["Id"]),
+                OrdersId = Convert.ToInt32(sqldatareader["OrdersId"]),
+                Product = Convert.ToString(sqldatareader["Product"]),
+                Description = Convert.ToString(sqldatareader["Description"]),
+                Quantity = Convert.ToInt32(sqldatareader["Quantity"]),
+                Price = Convert.ToDecimal(sqldatareader["Price"]),
+
+            };
+            sqldatareader.Close();
+            return user;
+
+        }
+
+
+
+        public List<Orderline> GetAllOrderLine()
+        {
+
+            var sql = "SELECT * from Orderlines;";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+
+            var sqldatareader = sqlcmd.ExecuteReader();
+            var orderline = new List<Orderline>();
+            while (sqldatareader.Read())
+            {
+                var id = Convert.ToInt32(sqldatareader["Id"]);
+                var orderid = Convert.ToInt32(sqldatareader["OrderId"]);
+                var product = Convert.ToString(sqldatareader["Product"]);
+                var description = Convert.ToString(sqldatareader["DEscription"]);
+                var quantity = Convert.ToInt32(sqldatareader["Quantity"]);
+                var price = Convert.ToDecimal(sqldatareader["Price"]);
+                var Orderline = new Orderline()
+                {
+                    Id = id,
+                    OrdersId = orderid,
+                    Product = product,
+                    Description = description,
+                    Quantity = quantity,
+                    Price = price
+                };
+                orderline.Add(Orderline);
+
+            }
+            sqldatareader.Close();
+            return orderline;
+
+        }
+
+
+
+
+        public bool Change(Orderline orderline)
+        {
+            var sql = $"Update Orderlines Set" +
+                " OrderId = @orderid, " +
+                "Product = @cityproduct, " +
+                "Description = @description, " +
+                "Quantity = @quantity, " +
+                "Price = @[rice, " +
+                "Where Id = @id;";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+            sqlcmd.Parameters.AddWithValue("@ordersid", orderline.OrdersId);
+            sqlcmd.Parameters.AddWithValue("@product", orderline.Product);
+            sqlcmd.Parameters.AddWithValue("@description", orderline.Description);
+            sqlcmd.Parameters.AddWithValue("@quantity", orderline.Quantity);
+            sqlcmd.Parameters.AddWithValue("@price", orderline.Price);
+            var rowsAffected = sqlcmd.ExecuteNonQuery();
+            return (rowsAffected == 1);
+
+
+        }
+
+
+
+        public bool Delete(Orderline orderline)
+        {
+            var sql = $"Delete from Customers" +
+                "Where Id = @id";
+            var sqlcmd = new SqlCommand(sql, connection.SqlConn);
+            sqlcmd.Parameters.AddWithValue("@id", orderline.Id);
+            var rowsAffected = sqlcmd.ExecuteNonQuery();
+            return (rowsAffected == 1);
+
+        }
 
 
 
